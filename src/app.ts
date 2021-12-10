2cbd36f293e696c1276f30f3385ac6bafda90114
Swejal-web <swejalshrestha08@gmail.com>
2021-12-10 12:26:31 +0545

    server running

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.write('Hey there');
+  res.end();
+});
+
 app.listen(3000, () => {
   console.log('Connected to port 3000');
 });
