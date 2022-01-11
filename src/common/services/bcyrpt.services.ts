905a0e1d9e53eec5a024dd77663380c98ff92278
Swejal-web <swejalshrestha08@gmail.com>
2022-01-11 19:09:16 +0545

    database config 2

--- src/common/services/bcyrpt.services.ts
+++ src/common/services/bcyrpt.services.ts
@@ -21,24 +21,10 @@ export class BcryptService {
 //     table.string('password').nullable();
 //     table.specificType('expiresIn', 'INTERVAL');
 //     table.timestamp('created_at').defaultTo(knex.fn.now());
-//     table.timestamp('updated_at').defaultTo(knex.fn.now());
-//   });
-// }
-
-// export async function down(knex: Knex): Promise<void> {
-//   return knex.schema.dropTable('secrets');
-// }
-
-// import { Knex } from 'knex';
-
-// export async function up(knex: Knex): Promise<void> {
-//   return knex.schema.table('secrets', (table) => {
 //     table.string('expires_at');
 //   });
 // }
 
 // export async function down(knex: Knex): Promise<void> {
-//   return knex.schema.table('secrets', (table) => {
-//     table.dropColumn('expires_at');
-//   });
+//   return knex.schema.dropTable('secrets');
 // }
--- test/secret/secret.e2e-spec.ts
+++ test/secret/secret.e2e-spec.ts
@@ -4,6 +4,9 @@
 
 // eslint-disable-next-line node/no-unpublished-import
 import request from 'supertest';
+import { v4 as uuid4 } from 'uuid';
+import { createExpiration } from 'secrets/exceptions';
+import { createSecret } from '../factory/secret';
 // eslint-disable-next-line import/no-unresolved
 import { AppFactory } from '../factory/app';
 
@@ -28,29 +31,33 @@ describe('Secret e2e', () => {
       })
       .expect(201);
 
-    expect(body).toEqual(
-      expect.objectContaining({
-        body: 'secret body goes here',
-        expiresIn: expect.objectContaining({
-          day: 1
-        })
-      })
-    );
+    // expect(body).toEqual(
+    //   expect.objectContaining({
+    //     secret: {
+    //       body: 'secret body goes here',
+    //       expiresAt: '2022-01-12T18:28:11.870+05:45',
+    //       id: '1833ea7a-21d8-4f1c-88a5-63a11fc4b787',
+    //       password:
+    //         '$2b$05$ilqf5qoxANTHx7LYTy.0fOrMvJTN.eudBE41s5v/4h8qmhoY15r9i',
+    //       expiresIn: expect.objectContaining({
+    //         day: 1
+    //       })
+    //     }
+    //   })
+    // );
   });
 
   // it('GET/secrets/:id should fetch entry', async () => {
   //   const id = uuid4();
   //   await createSecret({
-  //     id:'d01c1cd6-ea09-479a-acdd-2ebab1992bdd',
+  //     id,
   //     body: 'secret body goes here',
   //     password: 'secret',
-  //     expiresIn: '7 days',
-  //     expiresAt: '2022-01-11 06:52:36 +0000'
+  //     expiresIn: '1 days',
+  //     expiresAt: '2022-01-12T18:35:19.650+05:45'
   //   });
 
-  //   await request(app.instance)
-  //     .get(`/private/d01c1cd6-ea09-479a-acdd-2ebab1992bdd`)
-  //     .expect(200);
+  //   await request(app.instance).get(`/api/private/${id}`).expect(200);
   // });
 
   afterAll(async () => {
