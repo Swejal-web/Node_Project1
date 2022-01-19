6bd4dadb4e24725598ce48028d64dbf21501a0ff
Swejal-web <swejalshrestha08@gmail.com>
2022-01-19 15:20:38 +0545

    bug fix

--- frontend/zustandStore/store.ts
+++ frontend/zustandStore/store.ts
@@ -1,14 +1,10 @@
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { Isecret } from 'components/SecretForm';
 import { IError } from 'components/ErrorBox';
 
 const store = (set: any) => ({
-  secret: {},
   error: {},
-  addSecret: (secretData?: Isecret) => {
-    set((state: any) => ({ secret: { ...state.secret, secretData } }));
-  },
+
   addError: (errorData?: IError) => {
     set((state: any) => ({ error: { ...state.error, errorData } }));
   }
